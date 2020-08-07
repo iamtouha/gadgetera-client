2243a6bf15bcdad5e38fd055f9274b886af182ed
Touha Zohair <touha98@gmail.com>
2020-08-08 00:26:53 +0600

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-app-bar :hide-on-scroll="isMobile" app></v-app-bar>
+    <v-main>
+      <nuxt />
+    </v-main>
+    <client-only>
+      <v-bottom-navigation app v-if="isMobile"></v-bottom-navigation>
+    </client-only>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown
+    },
+  },
+})
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="product in products" :key="product.id" cols="12" sm="6" md="4">
+        <v-card>
+          <v-card-title>{{product.name}}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import productsQuery from '~/graphql/queries/products.gql'
+export default Vue.extend({
+  name: 'Products',
+  head: {
+    title: 'Shop',
+  },
+  data: () => ({
+    products: [],
+  }),
+  apollo: {
+    products: {
+      query: productsQuery,
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+</style>
