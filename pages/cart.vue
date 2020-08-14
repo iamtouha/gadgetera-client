903ad3e451f62a4a1cb1b6ca68c41cb6d219d484
Touha Zohair <touha98@gmail.com>
2020-08-14 16:49:10 +0600

    cart page added

---
+++ pages/cart.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h1 class="title">Cart</h1>
+    <v-row>
+      <v-col cols="12">
+        <!--  -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Cart',
+})
+</script>
