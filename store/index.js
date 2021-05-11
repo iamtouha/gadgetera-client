74ec0d38b5453f3b17a7c4603235c8b68dc442c3
Touha Zohair <touha98@gmail.com>
2021-05-11 22:33:46 +0600

    unexpected  field submission (_v) in address object fixed

--- store/index.js
+++ store/index.js
@@ -22,7 +22,22 @@ export const mutations = {
     state.token = jwt;
   },
   SET_ADDRESS(state, address) {
-    state.address = address;
+    const {
+      receiver,
+      district,
+      sub_district,
+      email,
+      phone,
+      street_address
+    } = address;
+    state.address = {
+      receiver,
+      district,
+      sub_district,
+      email,
+      phone,
+      street_address
+    };
   },
   LOG_OUT(state) {
     state.user = null;
@@ -46,23 +61,8 @@ export const actions = {
         const { id, email, name, address } = await $axios.$get("/users/me");
         commit("SET_USER", { id, email, name });
         if (address) {
-          const {
-            receiver,
-            district,
-            sub_district,
-            email,
-            phone,
-            street_address
-          } = await $axios.$get("/addresses/" + address);
-
-          commit("SET_ADDRESS", {
-            receiver,
-            district,
-            sub_district,
-            email,
-            phone,
-            street_address
-          });
+          const addressDoc = await $axios.$get("/addresses/" + address);
+          commit("SET_ADDRESS", addressDoc);
         }
       }
       commit("cart/SET_CART");
