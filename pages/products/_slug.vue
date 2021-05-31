15106274f4bea832dd8ff1cb0c94c2cd4037f083
Touha Zohair <touha98@gmail.com>
2021-05-31 23:27:51 +0600

    SEO improvements

--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -3,14 +3,17 @@
     <v-row>
       <v-col cols="12" sm="6">
         <transition name="fade" mode="out-in">
-          <v-img
-            :key="image.id"
-            max-width="400px"
-            class="mx-auto product-img rounded"
-            aspect-ratio="1"
-            :src="image.url"
-            :lazy-src="image.formats && image.formats.thumbnail.url"
-          />
+          <div :key="image.id" class="responsive rounded">
+            <div class="sizer">
+              <div class="wrapper">
+                <img
+                  :src="imageUrl"
+                  :alt="image.alternativeText"
+                  :title="image.caption"
+                />
+              </div>
+            </div>
+          </div>
         </transition>
         <v-row
           class="mt-2 mx-auto controller"
@@ -23,15 +26,18 @@
             cols="2"
             class="pa-1"
           >
-            <v-img
-              class="rounded cursor-pointer"
-              max-width="100%"
-              aspect-ratio="1"
-              :src="img.formats.thumbnail.url"
-              @click="image = img"
-            >
+            <div class="responsive rounded">
               <v-overlay :opacity="0.6" absolute :value="image.id === img.id" />
-            </v-img>
+              <div class="sizer">
+                <div class="wrapper">
+                  <img
+                    :src="img | thumb"
+                    :alt="img.alternativeText"
+                    @click="image = img"
+                  />
+                </div>
+              </div>
+            </div>
           </v-col>
         </v-row>
       </v-col>
@@ -39,10 +45,10 @@
         <h1 class="text-md-h4 text-sm-h5 text-h6 mb-3">
           {{ product.name }}
         </h1>
-        <h1 v-if="!product.discount" class="mb-3 text-h6 font-weight-bold">
+        <h2 v-if="!product.discount" class="mb-3 text-h6 font-weight-bold">
           {{ product.price | groupNum }}
-        </h1>
-        <h1 v-else class="text-body-1 d-flex">
+        </h2>
+        <h2 v-else class="text-body-1 d-flex">
           <span class="text-h6 font-weight-bold">
             {{
               Math.ceil(product.price - product.price * product.discount)
@@ -53,7 +59,7 @@
               {{ Math.ceil(100 * product.discount) }}% off)
             </span>
           </span>
-        </h1>
+        </h2>
         <p class="mt-4">
           {{ product.overview }}
         </p>
@@ -125,6 +131,12 @@ export default {
       }
       const formatter = new Intl.NumberFormat("en-US");
       return "৳ " + formatter.format(price);
+    },
+    thumb(img) {
+      if (!img.formats) {
+        return img.url;
+      }
+      return img.formats.thumbnail.url;
     }
   },
   data: () => ({
@@ -162,16 +174,34 @@ export default {
       meta: [
         {
           hid: "description",
-          name: "description",
+          property: "description",
           content: this.product.overview
         },
         {
-          hid: "og:image",
-          name: "og:image",
-          content: this.image.url
+          hid: "og:url",
+          property: "og:url",
+          content: `${process.env.BASE_URL ||
+            "https://gadgeterabd.com"}/products/${this.product.slug}`
+        },
+        { hid: "og:title", property: "og:title", content: this.product.name },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: this.product.overview
+        },
+        { hid: "og:image", property: "og:image", content: this.image.url },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image"
         }
       ]
     };
+  },
+  computed: {
+    imageUrl() {
+      return this.image.formats?.small?.url || this.image.url;
+    }
   },
   methods: {
     addToCart() {
@@ -224,6 +254,36 @@ export default {
   }
   .add2cart-row {
     justify-content: center;
+  }
+}
+</style>
+<style lang="scss" scoped>
+.responsive {
+  position: relative;
+  overflow: hidden;
+  .offer-chip {
+    position: absolute;
+    z-index: 4;
+    top: 5px;
+    right: 5px;
+  }
+  .sizer {
+    position: relative;
+    width: 100%;
+    padding-bottom: 100%;
+  }
+  .wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
   }
 }
 </style>
